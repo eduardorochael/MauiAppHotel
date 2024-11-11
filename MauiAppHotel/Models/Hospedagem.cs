4d21e167b3d6b9480445d17d84ab9020c29a748f
EDUARDO SILVA <eduardo.silva1305@etec.sp.gov.br>
2024-11-11 17:16:38 -0300

    ASDD

---
+++ MauiAppHotel/Models/Hospedagem.cs
@@ -0,0 +1,29 @@
+﻿namespace MauiAppHotel.Models
+{
+      public class Hospedagem
+    {
+        public Quarto QuartoSeleciondo {get; set;}
+        public int QntAdultos { get;set;}
+        public int QntCriancas { get; set;}
+        public DateTime DataChekin { get; set;}
+        public DateTime DataChekOut { get; set; }
+
+        public int Estadia
+        {
+            get => DataChekOut.Subtract(DataChekin).Days;
+        }
+        public double ValorTotal
+        {
+            get
+            {
+                double valor_adultos = QntAdultos * QuartoSeleciondo.ValorDiariaAdulto;
+                double valor_criancas = QntCriancas * QuartoSeleciondo.ValorDiariaCrianca;
+
+                double total = (valor_adultos + valor_criancas) * Estadia;
+
+                return total;
+
+            }
+        }
+    }
+}
--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppHotel.Models;
+
 namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
@@ -19,12 +21,25 @@ public partial class ContratacaoHospedagem : ContentPage
         dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddMonths(6);
     }
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
         try
         {
-            Navigation.PushAsync(new HospedagemContratada());
+            Hospedagem h = new Hospedagem
+            {
+                QuartoSeleciondo = (Quarto)pck_quarto.SelectedItem,
+                QntAdultos = Convert.ToInt32(stp_adultos.Value),
+                QntCriancas = Convert.ToInt32(stp_criancas.Value),
+                DataChekin = dtpck_checkin.Date,
+                DataChekOut = dtpck_checkout.Date,
 
+            };
+
+            await Navigation.PushAsync(new HospedagemContratada()
+            {
+                BindingContext = h
+            });
+            
         }
         catch (Exception ex)
         {
